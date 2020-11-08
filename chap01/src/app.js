4673750c4ad616b5291180896e8e0b8e6dc20d59
robertmoore40 <robertcmoore92@gmail.com>
2020-11-08 12:19:21 -0500

    added additional var imports

--- chap01/src/app.js
+++ chap01/src/app.js
@@ -2,3 +2,5 @@ var setPrototypeOf = require('setprototypeof')
 var methods = require('methods');
 var Router = require('./router');
 var Layer = require('./Layer')
+var slice = Array.prototype.slice;
+var http = require('http');

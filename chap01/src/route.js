64280e1b1ef9e0d75a1bf8d18dce44fe09f09140
robertmoore40 <robertcmoore92@gmail.com>
2020-11-10 08:11:12 -0500

    added function

--- chap01/src/route.js
+++ chap01/src/route.js
@@ -3,3 +3,11 @@ module.exports = Route;
 var methods = require('methods');
 var flatten = require('array-flatten');
 var Layer = require('./Layer')
+
+
+function Route(path) {
+    this.path = path;
+    this.stack = [];
+
+    this.methods = {}
+}

09b74f04d1955647b9765945ef7c9fb70e176e0d
robertmoore40 <robertcmoore92@gmail.com>
2020-11-10 08:05:36 -0500

    added app listen

--- chap01/src/app.js
+++ chap01/src/app.js
@@ -48,3 +48,8 @@ app.set = function set(setting,val) {
             }
         };
         
+
+        app.listen = function listen() {
+            var server = http.createServer(this);
+            return server.listen.apply(server, arguments);
+        };

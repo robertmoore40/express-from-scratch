88b2373a2c42431676214b334299777ef7fd61a9
robertmoore40 <robertcmoore92@gmail.com>
2020-11-12 09:59:57 -0500

    added proto

--- chap01/src/router.js
+++ chap01/src/router.js
@@ -1,3 +1,6 @@
 var setPrototypeOf = require('setprototypeof');
 var Route = require('./route');
 var Layer = require('./Layer');
+
+var proto = module.exports = function(options) {
+    var opts = options || {}

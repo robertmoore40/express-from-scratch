623f074a910d34e63f67368566df9b73c12b2619
robertmoore40 <robertcmoore92@gmail.com>
2020-11-15 18:38:23 -0500

    added express app get

--- chap02/index.js
+++ chap02/index.js
@@ -1,2 +1,8 @@
 let express = require('./src/express')
 const app = express()
+
+app.get('/', (req, res) => {
+    res.writeHead(200)
+    res.write('Hello express app from /');
+    res.end();
+});

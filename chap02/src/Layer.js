7685de3384568aa94d32636803a3bc02ff129a62
robertmoore40 <robertcmoore92@gmail.com>
2020-11-17 14:10:33 -0500

    added this handle params path

--- chap02/src/Layer.js
+++ chap02/src/Layer.js
@@ -4,3 +4,9 @@ function Layer(path, options, fn) {
     if (!(this instanceof Layer)) {
         return new Layer(path, options, fn);
     }
+
+    this.handle = fn;
+    this.name = fn.name || '<anonymous>';
+    this.params = undefined;
+    this.path = undefined;
+}

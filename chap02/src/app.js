28bacc1b2664975a46199d48ee8719ab4b67e5d0
robertmoore40 <robertcmoore92@gmail.com>
2020-11-17 14:15:21 -0500

    added switch and set

--- chap02/src/app.js
+++ chap02/src/app.js
@@ -20,3 +20,15 @@ app.init = function() {
 
 app.set = function set(setting,val) {
     this.settings[setting] = val;
+
+    switch (setting) {
+        case 'etag':
+            this.set('etag fn',"")
+            break;
+        case 'query parser':
+            this.set('query parser fn',"")
+            break
+        case 'trust proxy':
+            this.set('trust proxy fn',"");
+            break;
+    }

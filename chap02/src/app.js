99c98db697c029950284250bd361c8e4e2e8d5d3
robertmoore40 <robertcmoore92@gmail.com>
2020-11-18 12:27:45 -0500

    add listening on server for arguments

--- chap02/src/app.js
+++ chap02/src/app.js
@@ -45,3 +45,9 @@ app.lazyrouter = function lazyrouter() {
         this._router = new Router({})
     }
 };
+
+
+app.listen = function listen() {
+    var server = http.createServer(this);
+    return server.listen.apply(server, arguments);
+};

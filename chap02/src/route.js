8d2177532246d763ce813ae8e244b191c1342cfd
robertmoore40 <robertcmoore92@gmail.com>
2020-11-19 10:01:20 -0500

    added route

--- chap02/src/route.js
+++ chap02/src/route.js
@@ -4,3 +4,10 @@ var methods = require('methods');
 var flatten = require('array-flatten');
 
 var Layer = require('./Layer')
+
+function Route(path) {
+    this.path = path;
+    this.stack = [];
+
+    this.methods = {}
+}

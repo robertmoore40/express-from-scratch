4da2fb55586f70d609ac72043bcf1017606f612f
robertmoore40 <robertcmoore92@gmail.com>
2020-11-25 10:54:37 -0500

    trust proxy

--- chap04/src/app.js
+++ chap04/src/app.js
@@ -29,3 +29,8 @@ app.set = function set(setting,val) {
             case 'query parser':
             this.set('query parser fn',"")
             break
+
+            case 'trust proxy':
+                this.set('trust proxy fn',"");
+                break;
+        }

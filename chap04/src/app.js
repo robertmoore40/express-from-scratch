e02d496bba8e5d63dcfeb7bd6793be4505292189
robertmoore40 <robertcmoore92@gmail.com>
2020-11-25 10:54:11 -0500

    added switch

--- chap04/src/app.js
+++ chap04/src/app.js
@@ -20,3 +20,8 @@ app.init = function() {
 
 app.set = function set(setting,val) {
     this.settings[setting] = val;
+
+    switch (setting) {
+        case 'etag':
+            this.set('etag fn',"")
+            break;

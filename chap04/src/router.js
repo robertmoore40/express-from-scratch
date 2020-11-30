e0462a98a0364ba05618e47cd474f2160bc79142
robertmoore40 <robertcmoore92@gmail.com>
2020-11-29 21:00:56 -0500

    added proto and otps

--- chap04/src/router.js
+++ chap04/src/router.js
@@ -3,3 +3,6 @@ var Route = require('./route');
 
 var Layer = require('./Layer');
 var parseUrl = require('parseurl');
+
+var proto = module.exports = function(options) {
+    var opts = options || {}

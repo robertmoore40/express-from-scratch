fb9dfb436dd383799560faedee30c48022206d37
robertmoore40 <robertcmoore92@gmail.com>
2020-12-07 14:24:47 -0500

    finish switch

--- chap05/src/app.js
+++ chap05/src/app.js
@@ -30,3 +30,8 @@ app.set = function set(setting,val) {
             case 'query parser':
             this.set('query parser fn',"")
             break
+
+            case 'trust proxy':
+                this.set('trust proxy fn',"");
+                break;
+        }

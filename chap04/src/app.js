b1a2e85edb797d500d5a6cf33da3f989d565c0af
robertmoore40 <robertcmoore92@gmail.com>
2020-12-13 12:45:02 -0500

    added server return

--- chap04/src/app.js
+++ chap04/src/app.js
@@ -50,3 +50,5 @@ app.set = function set(setting,val) {
 
     app.listen = function listen() {
         var server = http.createServer(this);
+        return server.listen.apply(server, arguments);
+    };
